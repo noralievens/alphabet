7c6fc03f4f7572b052cc7bb00fd11412d8ca1480
Arno Lievens <arnolievens@gmail.com>
2021-09-12 21:04:30 +0200

    test mac

--- src/player.c
+++ src/player.c
@@ -101,15 +101,18 @@ double player_update(Player* this)
 
 void player_load_track(Player* this, Track* track, double position)
 {
-    const char *cmd[] = {"loadfile", track->uri, NULL};
+    if (position < 0) position = 0;
+    if (position) position += 0.050;
+
+    char posstr[15];
+    g_snprintf(posstr, sizeof(posstr)/sizeof(posstr[0]), "start=%f", position);
+    printf("posstr: %s\n", posstr);
+    /* while (*p) { *p = *p == ',' ? '.' : *p; p++; } */
+    /* printf("posstr: %s\n", posstr); */
+
+    const char *cmd[] = {"loadfile", track->uri, "replace", posstr, NULL};
     check_error(mpv_command(this->mpv, cmd));
     this->current = track;
-
-    /* FIXME */
-    /* must wait before sending goto command*/
-    /* compensate this in position */
-    g_usleep(50000);
-    player_goto(this, position + 0.05);
 }
 
 int player_event_handler(Player* this)
